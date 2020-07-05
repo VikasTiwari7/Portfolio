d3e5a4854fc65e81ff259896f0614b28c2c9942a
vikas <tvikas6523@gmail.com>
2020-07-05 18:17:58 +0530

    My Portfolio

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername="localhost:3308";
+$username="root";
+$password="";
+$dbname="curd";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn)
+{
+	
+}else
+{
+    
+}
+?>
---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+   
+    <title>Resume</title>
+</head>
+<body>
+    <header class="main">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-lg-4 col-md-4">
+                    <img src="images/vikas.png" alt="">
+                </div>
+            
+            <div class="col-lg-8 col-md-8">
+                <div class="d-flex flex-column">
+                    <div class="p-5 bg-dark text-white">
+                        <div class="d-flex flex-row justify-content-between align-items-center">
+                            <h1 class=""> Vikas Tiwari</h1>
+                            <!-- <p>9592704535</p> -->
+                            <div>
+                            <a href="https://www.facebook.com/tvikas45/">    
+                            <i class="fa fa-facebook"></i>
+                            </a>
+                            </div>
+                            <div><a href="https://www.facebook.com/" target="_blank"> <i class="fa fa-google-plus"></i></a></div>
+                            <div>
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <i class="fa fa-linkedin"  target="_blank"></a></i></div>
+                            <div>
+                                <a href="https://www.facebook.com/ " target="_blank"class=>
+                                <i class="fa fa-twitter" target="_blank"></i></a></div>
+                        </div>
+                    </div>
+                    <div class="p-3 text-white bg-black " >
+                        <h4>B.tech Student From Iet Bhaddal (Punjab Technical University)</h4> 
+                        <h5> Taking  6 months Internship from  IIT ROPAR </h5>
+
+                    </div>
+                    <div class="d-flex flex-row text-center text-white ">
+                        <div class="p-4 bg-success font-div" data-toggle="collapse" data-target="#home">
+                            <i class="fa fa-home fa-2x d-block"></i>Home
+                        </div>
+                        <div class="p-4 bg-danger font-div">
+                            <i class="fa fa-graduation-cap fa-2x d-block"  data-toggle="collapse" data-target="#resume"></i>Qualification
+                        </div>
+                        <div class="p-4 bg-primary font-div">
+                            <i class="fa fa-folder  fa-2x d-block"  data-toggle="collapse" data-target="#work"></i>Work
+                        </div>
+                        <div class="p-4 bg-warning font-div">
+                            <i class="fa fa-envelope  fa-2x d-block"  data-toggle="collapse" data-target="#contact"></i>Contact
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </header>
+    <div class="container">
+        <div class="collapse" id="home">
+
+            <div class="p-5 text-white bg-success">
+                <h2>My Objective</h2>
+              <p>A Computer Science professional seeking challenging opportunity to work for an organization
+                  to enhance my knowledge, skills & techniques which can benefits the organization. 
+              </p>
+
+            </div>
+            <div class="card card-body">
+                <h2>My Technical Skills</h2>
+                <p>I am a computer science student so following are  my technical skills realted to my degree as well as my traning periods</p>
+                <h4>HTML & CSS</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:80%">80%
+                    </div>
+                </div>
+                <h4>Bootstrap 4</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%">70%
+                    </div>
+                </div>
+                <h4>JQuery & JavaScript</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:75%">75%
+                    </div>
+                </div>
+                <h4>PHP</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:80%">80%
+                    </div>
+                </div>
+                <h4>Reactjs & Nodejs</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:65%">65%
+                    </div>
+                </div>
+                <h4>Java</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:80%">80%
+                    </div>
+                </div>
+                <h4>Python</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:40%">40%
+                    </div>
+                </div>
+                <h4> C & C++</h4>
+                <div class="progress mb-4">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:35%">35%
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="collapse" id="resume">
+            <div class="p-5 text-white bg-danger">
+                <h2>My Qualification</h2>
+                <h4>+2 (Non-Medical) from S.V.M, Morinda(P.S.E.B.)</h4>
+                <h5>10th from S.V.M, Morinda(P.S.E.B.)</h5>
+                <h6>Taking 2 Months  java training from H.P.(Mohali) </h6>
+            </div>
+            <div class=" card card-body">
+                <h2>Where I Have Worked</h2>
+                <p>I am worked in following companies at differnt positions that are following them:-</p>
+                <div class="card-deck text-center">
+                    <div class="card">
+                        <div class="card-header Text-uppercase font-weight-bold">
+                            JavaDevelopment
+                        </div>
+                        <div class="card-body">
+                            <p>I am working in IIT Ropar startup company name Scratchnest as a java developer, while doing job there a make a alot of projects these are folowing them, i used javafx for the fronthand JavaDevelopment and mysql as a backdend developlment</p>
+                            <ul>
+                                <li>Attendance System</li>
+                                <li>Army managagement System</li>
+                                <li>Dynamically curd operation System using java  </li>
+
+                            </ul>
+                            <p class="bg-dark text-white p-2">As A Front_End Developer</p>
+                            <p class="bg-dark text-white p-2">As A Backend_End Developer</p>
+                        </div>
+                        <div class="card-footer">Jan/2020- to till now</div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header Text-uppercase font-weight-bold">
+                            WebDeveloper
+                        </div>
+                        <div class="card-body">
+                            
+                            <p>while doing my degree i worked as a freelancer, and doing alot of project specially in website design and develpoment these kind of websites design are folowing them:- </p>
+                            <ul>
+                                <li>  Resume Websites</li>
+                                <li> E-commerce websites</li>
+                                <li>Personal portfolio blogs </li>
+                                <li>Mp4 to mp3 conversion  </li>
+                                <li>Typing speed websites </li>
+                            </ul>
+                            <p class="bg-dark text-white p-2">As A Front_End Developer</p>
+                            <p class="bg-dark text-white p-2">As A Backend_End Developer</p>
+                        </div>
+                        <div class="card-footer">Sept/2019</div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header Text-uppercase font-weight-bold">
+                            Others
+                        </div>
+                        <div class="card-body">
+                           <ul>
+                               <li>International call-center Sprial Enfotech as network executive</li>
+                               <li>Sawarj Agency as data-operator</li>
+                               <li>On-dot courier service as a data-operator</li>
+                           </ul>
+                            <!-- <p class="bg-dark text-white p-2">As A Front_End Developer</p>
+                            <p class="bg-dark text-white p-2">As A Front_End Developer</p> -->
+                        </div>
+                        <div class="card-footer">2018-july/2019</div>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+        <div class="collapse" id="work">
+            <div class="p-5 text-white bg-primary">
+                <h2>Personal skills</h2>
+               <ul>
+                   <li>Good Communication & presentation skills.</li>
+                   <li>Honest, hardworking & eager to learn.</li>
+                   <li>Ability to change with the changing environment.</li>
+               </ul>
+            </div>
+            <div class="card card-body">
+                <h2>What I Have built??</h2>
+                <p>These are the few pictures of my projects that are mentioned below. </p>
+
+
+                <div class="row no-gutters">
+                    <div class="col-lg-4"><img src="images/food1.PNG" alt=""></div>
+                    <div class="col-lg-4"><img src="images/gym1.PNG" alt=""></div>
+                    <div class="col-lg-4"><img src="images/food1.PNG" alt=""></div>
+                    <div class="col-lg-4 order=5"><img src="images/gym1.PNG" alt=""></div>
+                    <div class="col-lg-4"><img src="images/food1.PNG" alt=""></div>
+                    <div class="col-lg-4"><img src="images/gym1.PNG" alt=""></div>
+                </div>
+              
+            </div>
+
+
+
+        </div>
+        <div class="collapse" id="contact">
+            <div class="p-5 text-white bg-warning">
+                <h2>Contact Me</h2>
+                <p>#98 Ward no.9 Rest House Morinda</p>
+                <p>140101</p>
+                <p>9592704535</p>
+                <p>tvikas6523@gmail.com</p>
+            </div>
+            <div class=" card card-body">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 col-md-8 col-12">
+                        <div class="ml-5"><h1>Send Me  Message</h1></div>
+                        <form action=""method="POST">
+
+                            <div class="form-group">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-addon bg-warning text-white p-3">
+                                        <i class="fa fa-user"></i>
+                                    </span>
+                                    <input type="text"name="name" class="form-control bg-dark text-white p-3" placeholder="Name">
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-addon bg-warning text-white p-3">
+                                        <i class=" fa-3x fa fa-envelope"></i>
+                                    </span>
+                                    <input type="text"name="email" class="form-control bg-dark text-white p-3" placeholder="Email">
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-addon bg-warning text-white p-3">
+                                        <i class="fa fa-envelope"></i>
+                                    </span>
+                                    <input type="text"name="message" class="form-control bg-dark text-white p-3" placeholder="Message">
+                                </div>
+
+                            </div>
+                            <input type="submit" name="submit" class="btn btn-danger">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+    <div class="container">
+    <footer class="p-5 bg-dark text-center">
+        <button class="btn text-white"> <i class="fa fa-download  fa-2x d-block down "></i> Download</button>
+
+    </footer>
+    </div>
+    
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <script >
+    $('.font-div').click(function(){
+        $('.collapse').collapse('hide');
+    });
+    </script>
+</body>
+</html>
+
+<?php
+include 'connection.php';
+
+if(isset($_POST['submit']))
+{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $message1=$_POST['message'];
+
+   
+	// Authorisation details.
+	$username = "tmike04535@gmail.com";
+	$hash = "311a597a72d26980d9aa8ca8ca911c27c74d6d377a76e0896b1161a1a60bd274";
+
+	// Config variables. Consult http://api.textlocal.in/docs for more info.
+	$test = "0";
+
+	// Data for text message. This is the text message data.
+	$sender = "TXTLCL"; // This is who the message appears to be from.
+	$numbers = "9592704535"; // A single number or a comma-seperated list of numbers
+	$message = "$name\n.$email\n.$message1";
+	// 612 chars or less
+	// A single number or a comma-seperated list of numbers
+	$message = urlencode($message);
+	$data = "username=".$username."&hash=".$hash."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
+	$ch = curl_init('http://api.textlocal.in/send/?');
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch); // This is the result from the API
+    curl_close($ch);
+    
+
+    $insertquery="insert into resume(name,email,message) values ('$name','$email','$message1')";
+   
+   
+    $res=mysqli_query($conn , $insertquery);
+   
+    if($res){
+        ?>
+     <script>
+         alert("Thanks for giving the pricelss feedback");
+     </script>
+        <?php
+     
+    }else
+    {
+     ?>
+      <script>
+          alert("Data not inserted");
+      </script>
+         <?php
+      
+    }
+}
+
+?>
